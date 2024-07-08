33e683200aa16e94ec5586710df1327bf3cf1938
Evan Daniel Mikrut <mikrutevan@gmail.com>
2024-07-08 15:52:35 -0400

    removed sharp

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -24,7 +24,7 @@ const ProjectsPage: React.FC<PageType & { globals: any; projects: Projects }> = 
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const pageData = await fetchPageData('projects')
+  const pageData = { layout: [] } //await fetchPageData('projects')
   return {
     props: {
       ...pageData,
