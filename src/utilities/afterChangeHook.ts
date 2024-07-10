f1d27f4b5aab669b35773c3b16b4a4f89542e7bc
Evan Daniel Mikrut <mikrutevan@gmail.com>
2024-07-10 14:27:00 -0400

    ike

--- src/utilities/afterChangeHook.ts
+++ src/utilities/afterChangeHook.ts
@@ -4,16 +4,10 @@ import { CollectionAfterChangeHook } from 'payload';
 const afterChangeHook: CollectionAfterChangeHook = async () => {
   try {
     // Fetch projects data from the Payload "projects" collection
-    const projects = await payload.find({
-      collection: 'projects',
-    });
-
     const skills = await payload.find({
       collection: 'skillsCollection',
     })
 
-    // Do something with the fetched projects data
-    console.log('Fetched projects:', projects);
     console.log('Fetched skills:', skills);
 
     // You can further process the data or pass it to another function for rendering
